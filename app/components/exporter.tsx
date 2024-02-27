0e5435460dd36b1fd9ade03c32db575c77603b8f
Jiabo Zhang <jiabozhang@Jiabos-MacBook-Pro.local>
2024-02-27 18:25:59 +0800

    更新title为AI小助手

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -544,10 +544,8 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
-            <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
-            </div>
+            <div className={styles["main-title"]}>AI小助手</div>
+            <div className={styles["sub-title"]}></div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
               <span className={styles["icon-space"]}>&</span>
